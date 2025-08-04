4a420a94b6ae4a57ed2910330987427a60e9ffdb
Colby Ziyu Wang <112201493+colbyziyuwang@users.noreply.github.com>
2025-08-04 11:32:02 +0800

    Update lagrange_tuning.py (using git web)

--- lagrange_tuning.py
+++ lagrange_tuning.py
@@ -164,9 +164,9 @@ if __name__ == "__main__":
                     elif (METHOD == "bisection"):
                         if (abs(Q.mean() - eta) < threshold):
                             break
-                        elif (Q.mean() > eta):
-                            lambda_max_t = lambda_t
                         elif (Q.mean() < eta):
+                            lambda_max_t = lambda_t
+                        elif (Q.mean() > eta):
                             lambda_min_t = lambda_t
                         lambda_t = 0.5 * (lambda_min_t + lambda_max_t)
                     else: # baseline
